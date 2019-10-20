e8c57b8f2342751d4b666ae5fdb8822b423f5b9f
Venkatesh Pulletikurthi <venkatpulletikurthi@gmail.com>
2019-10-19 23:27:29 -0400

    added reference to the code

--- compressor_function.m
+++ compressor_function.m
@@ -9,6 +9,14 @@ function [mass_out,exergy_efficiency] = compressor_function(Power_in,Temp_in,Tem
 % Compressor_efficiency = efficiency of the compressor used (dimensionless <1)
 % Q_recovered = energy recovery in the units of Watts
 
+%References used:
+
+% Exergy Analysis of Industrial Air Compression 
+%(https://www.researchgate.net/publication/44209817_Exergy_Analysis_of_Industrial_Air_Compression?
+%...cont...
+%....enrichId=rgreq-ae4380060bf3922d38ce6a7e82d7d575-XXX&enrichSource=Y292ZXJQYWdlOzQ0MjA5ODE3O0FTOjExODgzMTc3OTQyMjIxM0AxNDA1MzQzMTEyNzQ2&el=1_x_2&_esc=publicationCoverPdf
+
+
 enthalpy_out = py.CoolProp.CoolProp.PropsSI('H','T',Temp_in,'P',P_in,'R744');
 enthalpy_in = py.CoolProp.CoolProp.PropsSI('H','T',Temp_out,'P',P_out,'R744');
 
