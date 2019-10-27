879cf49e78d42fe5bf997ac5295b3750b8f3918c
Venkatesh Pulletikurthi <venkatpulletikurthi@gmail.com>
2019-10-27 15:56:53 -0400

    generated sample plot

--- compressor_function.m
+++ compressor_function.m
@@ -1,4 +1,4 @@
-function [mass_out] = compressor_function(Power_in,Temp_in,Temp_out,P_in,P_out,compressor_efficiency)
+function [mass_out,Temp_out] = compressor_function(Power_in,Temp_in,P_in,P_out,compressor_efficiency)
 
 %Inputs:
 % Power_in and Q_recovery as float in Watts
@@ -17,23 +17,23 @@ function [mass_out] = compressor_function(Power_in,Temp_in,Temp_out,P_in,P_out,c
 % Compressor_efficiency = efficiency of the compressor used (dimensionless <1)
 % Q_recovered = energy recovery in the units of Watts
 
-enthalpy_out = zeros(length(Temp_in));
-enthalpy_in = zeros(length(Temp_in));
-entropy_out = zeros(length(Temp_in));
-entropy_in = zeros(length(Temp_in));
-mass_out = zeros(length(Temp_in));
+enthalpy_out = zeros(length(Power_in));
+enthalpy_in = zeros(length(Power_in));
+entropy_in = zeros(length(Power_in));
+T_out = zeros(length(Power_in));
+M_out = zeros(length(Power_in));
 
 
-for i=1:length(Temp_in)
-    enthalpy_in(i) = py.CoolProp.CoolProp.PropsSI('H','T',Temp_in(i),'P',P_in(i),'R744');
-    enthalpy_out(i) = py.CoolProp.CoolProp.PropsSI('H','T',Temp_out(i),'P',P_out(i),'R744');
-
-    entropy_in(i) = py.CoolProp.CoolProp.PropsSI('S','T',Temp_in(i),'P',P_in(i),'R744');
-    entropy_out(i) = py.CoolProp.CoolProp.PropsSI('S','T',Temp_out(i),'P',P_out(i),'R744');
-
-    mass_out(i) = (compressor_efficiency*Power_in)/(enthalpy_out(i)-enthalpy_in(i));
+for i=1:length(Power_in)
+    enthalpy_in(i) = py.CoolProp.CoolProp.PropsSI('H','T',Temp_in,'P',P_in,'R744');
+    entropy_in(i) = py.CoolProp.CoolProp.PropsSI('S','T',Temp_in,'P',P_in,'R744');
+      
+    enthalpy_out(i) = py.CoolProp.CoolProp.PropsSI('H','S',entropy_in(i),'P',P_out,'R744');
+    T_out(i) =  py.CoolProp.CoolProp.PropsSI('T','S',entropy_in(i),'P',P_out,'R744');
+    %isentropic compression
+    M_out(i) = (compressor_efficiency*Power_in(i))/(enthalpy_out(i)-enthalpy_in(i));  
 end
-
-
+    mass_out = M_out(:,1);
+    Temp_out = T_out(:,1);
 end
 
