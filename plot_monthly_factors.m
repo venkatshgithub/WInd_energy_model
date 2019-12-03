8acfd0bda90d38af837e889c34952754701f0b76
U-LAPTOP-ICC1R9SN\jkj00 <jjoffe@purdue.edu>
2019-12-03 17:34:10 -0500

    update for v

--- plot_monthly_factors.m
+++
@@ -1,8 +0,0 @@
-function plot_monthly_factors(monthly_factors)
-
-%monthly_factors.(intermit or storage).(avail or cap)
-
-figure
-plot(monthly_factors.storage.avail)
-set(gca,'xtick',1:12,'xticklabel',...
-     {'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'}) 
